ca4b41828f054fd4e0684b72fbb8bee7040742d4
Dave Kerr <dwmkerr@gmail.com>
2024-02-05 04:35:24 +0000

    refactor: initial setup

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # lifeline
+
 Visualise and track key life events with an interactive timeline
+
+## Developer Guide
+
+| Command | Description |
+| ------- | ----------- |
+| npm run lint | Lint the code with eslint/prettier |
+| npm run lint:fix | Fix the code with eslint/prettier |
+
+
+## Goals
+
+- Build multiple timelines of key life events
+- Events might be: where I lived, work, travel, holidays, climbing, medical, etc
+- Events can be major or minor, to allow a high level view or detailed view
+- Filter that lets you choose which lifelines to show
+
+## Building the line
+
+Could be text based:
+
+```
+2021-01-01: home - *moved to UK*
+2021-02-03: home - lived in US for a week
+```
+
+## TODO
+
+Quick and dirty task list:
+
+- [ ] refactor: initial page structure with joyui
---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/joy/Grid";
+import Stack from "@mui/joy/Stack";
+
+import BasicTimeline from "./components/BasicTimeline";
+import NavBar from "./components/NavBar";
+import RentalCard from "./components/RentalCard";
+import HeaderSection from "./components/HeaderSection";
+import Search from "./components/Search";
+import Filters from "./components/Filters";
+import Pagination from "./components/Pagination";
+
+const materialTheme = materialExtendTheme();
+
+export default function RentalDashboard() {
+  return (
+    <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+      <JoyCssVarsProvider>
+        <CssBaseline enableColorScheme />
+        <CssBaseline />
+        <NavBar />
+        <Grid
+          component="main"
+          container
+          sx={{
+            height: "calc(100vh - 55px)", // 55px is the height of the NavBar
+            gridTemplateRows: "auto 1fr auto",
+          }}
+        >
+          <Grid md={12}>
+            <Stack
+              sx={{
+                backgroundColor: "background.surface",
+                px: { xs: 2, md: 4 },
+                py: 2,
+                borderBottom: "1px solid",
+                borderColor: "divider",
+              }}
+            >
+              <HeaderSection />
+              <Search />
+            </Stack>
+          </Grid>
+          <Grid md={12}>
+            <Stack
+              spacing={2}
+              sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}
+            >
+              <Filters />
+              <Stack spacing={2} sx={{ overflow: "auto" }}>
+                <BasicTimeline />
+              </Stack>
+            </Stack>
+          </Grid>
+          <Pagination />
+        </Grid>
+      </JoyCssVarsProvider>
+    </MaterialCssVarsProvider>
+  );
+}
