2e033c88afea18ab5a73fa18ca5105cde6019333
Dave Kerr <dwmkerr@gmail.com>
2024-02-07 06:43:48 +0000

    test: fix tests

--- src/lib/LifelineCsv.test.ts
+++ src/lib/LifelineCsv.test.ts
@@ -2,22 +2,24 @@ import { importCsv } from "./LifelineCsv";
 
 describe("LifelineCsv", () => {
   describe("import", () => {
-    test("can import basic CSV file", () => {
-      const csv = `Year,Month,Day,Age,Category,Title,Notes (italic = mum's notes),,,,,,,,,,,,,,,,,,,,,,
+    test("can import basic CSV file", async () => {
+      const csv = `Year,Month,Day,Age,Category,Title,Notes,,,,,,,,,,,,,,,,,,,,,,
 1985,1,,,,"Belfast",,,,,,,,,,,,,,,,,,,,,,,
-1987,2,,,,Colchester,,,,,,,,,,,,,,,,,,,,,,,,
+1987,2,,,,Colchester,,,,,,,,,,,,,,,,,,,,,,,
 `;
-      const lifeEvents = importCsv(csv);
-      expect(lifeEvents).toEqual([
+      const lifeEvents = await importCsv(csv);
+      expect(lifeEvents).toMatchObject([
         {
           year: 1985,
           month: 1,
           title: "Belfast",
+          notes: "",
         },
         {
           year: 1987,
           month: 2,
           title: "Colchester",
+          notes: "",
         },
       ]);
     });
--- src/lib/LifelineCsv.ts
+++ src/lib/LifelineCsv.ts
@@ -1,5 +1,49 @@
+import { parse } from "csv-parse";
 import { LifeEvent } from "./LifeEvent";
+import { LifelineError } from "./Errors";
 
-export function importCsv(csv: string): LifeEvent[] {
-  return [];
+type ImportedLifeEvent = Omit<LifeEvent, "id" | "userId">;
+
+export async function importCsv(csv: string): Promise<ImportedLifeEvent[]> {
+  const requireString = (
+    record: Record<string, string>,
+    colName: string,
+    line: number,
+  ) => {
+    const val = record[colName];
+    if (val === undefined) {
+      throw new LifelineError(
+        "CSV Error",
+        `Missing field ${colName} on line ${line}`,
+      );
+    }
+    return val;
+  };
+  const parser = parse(csv, {
+    columns: true,
+  });
+  const records: ImportedLifeEvent[] = [];
+  let line = 0;
+  for await (const record of parser) {
+    // Work with each record
+    console.log(record);
+    const year = Number.parseInt(requireString(record, "Year", line));
+    const month = record?.Month;
+    const day = record?.Day;
+    const category = record?.Category;
+    const title = record?.Title;
+    const notes = record?.Notes;
+
+    records.push({
+      date: new Date(),
+      year,
+      month: month ? Number.parseInt(month) : null,
+      day: day ? Number.parseInt(day) : null,
+      category,
+      title,
+      notes,
+    });
+    line = line + 1;
+  }
+  return records;
 }
